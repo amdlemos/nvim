770919ef13186877331fa0b123886006ecf0f8ce
amdlemos <amdlemos@gmail.com>
2024-12-24 16:00:59 -0300

    added ai

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    enabled = false,
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  },
+  "github/copilot.vim",
+}
