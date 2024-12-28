e992a3cf8a5d60cfa3e175bee670af023e8dc069
amdlemos <amdlemos@gmail.com>
2024-12-28 03:59:04 -0300

    wip

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,10 +1,9 @@
+local icons = require("plugins.lib.icons")
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    preset = "classic",
   },
   keys = {
     { "[", "", desc = "Prev" },
